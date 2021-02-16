cc7dfd2a78ad9aefb715c1f7b4920660709934d5
zero88 <sontt246@gmail.com>
2021-02-16 19:07:31 +0700

    Fix build

--- plugin/src/main/kotlin/io/github/zero88/qwe/gradle/generator/task/SystemdServiceExtension.kt
+++ plugin/src/main/kotlin/io/github/zero88/qwe/gradle/generator/task/SystemdServiceExtension.kt
@@ -1,20 +1,30 @@
 package io.github.zero88.qwe.gradle.generator.task
 
 import org.gradle.api.model.ObjectFactory
+import org.gradle.api.tasks.Input
 import org.gradle.kotlin.dsl.listProperty
 import org.gradle.kotlin.dsl.mapProperty
 import org.gradle.kotlin.dsl.property
 
 @Suppress("UnstableApiUsage") open class SystemdServiceExtension(objects: ObjectFactory) {
 
+    @Input
     val enabled = objects.property<Boolean>().convention(true)
+    @Input
     val javaPath = objects.property<String>().convention("/usr/bin/java")
+    @Input
     val arch = objects.property<Arch>()
+    @Input
     val jvmProps = objects.listProperty<String>()
+    @Input
     val systemProps = objects.listProperty<String>()
+    @Input
     val serviceName = objects.property<String>()
+    @Input
     val workingDir = objects.property<String>()
+    @Input
     val configFile = objects.property<String>()
+    @Input
     val params = objects.mapProperty<String, String>()
 
     enum class Arch {
