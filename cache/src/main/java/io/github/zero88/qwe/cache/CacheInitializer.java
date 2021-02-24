6e5a580499c5ffcc5e00aeb3e8c49ec941bf250a
zero88 <sontt246@gmail.com>
2021-02-24 10:21:42 +0700

    Use SharedDataLocalProxy as context in CacheInitializer

--- cache/src/main/java/io/github/zero88/qwe/cache/CacheInitializer.java
+++ cache/src/main/java/io/github/zero88/qwe/cache/CacheInitializer.java
@@ -1,9 +1,20 @@
 package io.github.zero88.qwe.cache;
 
+import java.util.function.Supplier;
+
+import io.github.zero88.qwe.component.SharedDataLocalProxy;
+
 import lombok.NonNull;
 
-public interface CacheInitializer<R extends CacheInitializer, C> {
+public interface CacheInitializer<R extends CacheInitializer> {
 
-    @NonNull R init(@NonNull C context);
+    @NonNull R init(SharedDataLocalProxy context);
+
+    default <T> void addBlockingCache(@NonNull SharedDataLocalProxy context, @NonNull String cacheKey,
+                                      @NonNull Supplier<T> blockingCacheProvider) {
+        context.getVertx()
+               .executeBlocking(future -> future.complete(blockingCacheProvider.get()),
+                                result -> context.addData(cacheKey, result.result()));
+    }
 
 }
