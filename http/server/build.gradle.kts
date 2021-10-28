8d7e890cbc5816378dab0a7954897e34ffe3cb77
zero88 <sontt246@gmail.com>
2021-10-28 08:12:25 +0700

    http server temp

--- http/server/build.gradle.kts
+++ http/server/build.gradle.kts
@@ -5,10 +5,21 @@ dependencies {
     
     compileOnly(project(":micro:discovery"))
     compileOnly(VertxLibs.serviceDiscovery)
+    compileOnly(VertxLibs.jwt)
+    compileOnly(VertxLibs.ldap)
+    compileOnly(VertxLibs.oauth2)
+    compileOnly(VertxLibs.shiro)
+    compileOnly(VertxLibs.webauthn)
 
     testImplementation(VertxLibs.junit)
     testImplementation(VertxLibs.webClient)
     testImplementation(testFixtures(project(":qwe-core")))
     testImplementation(project(":micro:discovery"))
     testImplementation(project(":http:client"))
+
+    testImplementation(VertxLibs.jwt)
+    testImplementation(VertxLibs.ldap)
+    testImplementation(VertxLibs.oauth2)
+    testImplementation(VertxLibs.shiro)
+    testImplementation(VertxLibs.webauthn)
 }
