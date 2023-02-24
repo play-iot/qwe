277a20ea704bb96f9ad58a717b4aaff3a032606c
zero88 <sontt246@gmail.com>
2023-02-24 22:20:36 +0700

    [:core] Correct namespace

--- core/src/main/java/cloud/playio/qwe/eventbus/package-info.java
+++ core/src/main/java/cloud/playio/qwe/eventbus/package-info.java
@@ -1,4 +1,4 @@
-@ModuleGen(name = "io-zero88-qwe-eventbus", groupPackage = "cloud.playio.qwe.eventbus")
+@ModuleGen(name = "cloud-playio-qwe-eventbus", groupPackage = "cloud.playio.qwe.eventbus")
 package cloud.playio.qwe.eventbus;
 
 import io.vertx.codegen.annotations.ModuleGen;
